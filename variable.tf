ad873e462c3e5cb73524e40ede13761e93212eb2
OluAd <74728792+OluAd@users.noreply.github.com>
2023-10-12 05:58:01 +0400

    sns file created

--- variable.tf
+++ variable.tf
@@ -85,3 +85,10 @@ variable "ssl_certificate_arn" {
     description = "ssl certificate arn"
     type        = string
 }
+
+#sns topic variable
+variable "operator_email" {
+    default     = "olubukoladio@gmail.com"
+    description = "a valid email address"
+    type        = string
+}
